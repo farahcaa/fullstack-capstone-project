513a0bf210870dee2a85639b4d5ae3d81d2cc639
Chris Farah <farahca@mail.uc.edu>
2024-07-02 20:41:23 -0400

    update4

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import SearchPage from './components/SearchPage/SearchPage';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import SearchPage from "./components/SearchPage/SearchPage";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+
 function App() {
   const navigate = useNavigate();
+  useEffect(() => {
+    const authtoken = sessionStorage.getItem("auth-token");
+    if (!authtoken) {
+      navigate("/app/login");
+    } else {
+      fetchUserProfile();
+    }
+  }, [navigate]);
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/search" element={<SearchPage/>} />
-        </Routes>
-        </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+      </Routes>
+    </>
   );
 }
 export default App;
